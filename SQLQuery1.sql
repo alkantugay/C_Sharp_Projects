6e7650fb15c3ed91ef9d6f929920308689527986
Tugay <51509025+alkantugay@users.noreply.github.com>
2020-03-27 18:25:33 +0300

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+create table TBLDERS (
+OGRID smallint,
+OGRNUMARA char(4),
+OGRAD varchar(15),
+OGRSOYAD varchar(20),
+OGRS1 tinyint,
+OGRS2 tinyint,
+OGRS3 tinyint,
+ORTALAMA decimal(18,2),
+DURUM bit)
