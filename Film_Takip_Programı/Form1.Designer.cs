8b11a5ebd47479e44041b9c6fab92db1ec58da56
Tugay <51509025+alkantugay@users.noreply.github.com>
2020-03-30 01:10:14 +0300

    Add files via upload

---
+++ Film_Takip_Programı/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Film_Listem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_AddMovie = new System.Windows.Forms.Button();
+            this.btn_EditMovie = new System.Windows.Forms.Button();
+            this.btn_ShowMovie = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_FindMovie = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(962, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_AddMovie
+            // 
+            this.btn_AddMovie.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_AddMovie.Location = new System.Drawing.Point(770, 100);
+            this.btn_AddMovie.Name = "btn_AddMovie";
+            this.btn_AddMovie.Size = new System.Drawing.Size(175, 45);
+            this.btn_AddMovie.TabIndex = 15;
+            this.btn_AddMovie.Text = "Add Movie";
+            this.btn_AddMovie.UseVisualStyleBackColor = true;
+            this.btn_AddMovie.Click += new System.EventHandler(this.btn_AddMovie_Click);
+            // 
+            // btn_EditMovie
+            // 
+            this.btn_EditMovie.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_EditMovie.Location = new System.Drawing.Point(770, 150);
+            this.btn_EditMovie.Name = "btn_EditMovie";
+            this.btn_EditMovie.Size = new System.Drawing.Size(175, 45);
+            this.btn_EditMovie.TabIndex = 16;
+            this.btn_EditMovie.Text = "Edit Movie";
+            this.btn_EditMovie.UseVisualStyleBackColor = true;
+            this.btn_EditMovie.Click += new System.EventHandler(this.btn_EditMovie_Click);
+            // 
+            // btn_ShowMovie
+            // 
+            this.btn_ShowMovie.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ShowMovie.Location = new System.Drawing.Point(770, 50);
+            this.btn_ShowMovie.Name = "btn_ShowMovie";
+            this.btn_ShowMovie.Size = new System.Drawing.Size(175, 45);
+            this.btn_ShowMovie.TabIndex = 17;
+            this.btn_ShowMovie.Text = "Show Movie List";
+            this.btn_ShowMovie.UseVisualStyleBackColor = true;
+            this.btn_ShowMovie.Click += new System.EventHandler(this.btn_ShowMovie_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(958, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(487, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "\" The more successful the villain, the more successful the picture. \" Alfred Hitc" +
+    "hcock";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 312);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 10;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1433, 393);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Movie Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 17);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Type:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Imdb:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 17);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "My Rating:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(353, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 17);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Description:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 23);
+            this.textBox1.TabIndex = 26;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 23);
+            this.textBox2.TabIndex = 27;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(106, 147);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 23);
+            this.textBox3.TabIndex = 28;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(106, 197);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 23);
+            this.textBox4.TabIndex = 29;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(106, 247);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(195, 23);
+            this.textBox5.TabIndex = 30;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(442, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(273, 120);
+            this.richTextBox1.TabIndex = 31;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_FindMovie
+            // 
+            this.btn_FindMovie.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_FindMovie.Location = new System.Drawing.Point(770, 200);
+            this.btn_FindMovie.Name = "btn_FindMovie";
+            this.btn_FindMovie.Size = new System.Drawing.Size(175, 45);
+            this.btn_FindMovie.TabIndex = 32;
+            this.btn_FindMovie.Text = "Find Movie";
+            this.btn_FindMovie.UseVisualStyleBackColor = true;
+            this.btn_FindMovie.Click += new System.EventHandler(this.btn_FindMovie_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Clear.Location = new System.Drawing.Point(770, 251);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(175, 44);
+            this.btn_Clear.TabIndex = 34;
+            this.btn_Clear.Text = "Clear All Textboxes";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(1457, 720);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_FindMovie);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_ShowMovie);
+            this.Controls.Add(this.btn_EditMovie);
+            this.Controls.Add(this.btn_AddMovie);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Movie List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_AddMovie;
+        private System.Windows.Forms.Button btn_EditMovie;
+        private System.Windows.Forms.Button btn_ShowMovie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_FindMovie;
+        private System.Windows.Forms.Button btn_Clear;
+    }
+}
+
---
+++ Film_Takip_Programı/Form1.cs
@@ -0,0 +1,149 @@
+﻿/*
+ * OleDb Delete işlemine izin vermiyor. Sadece düzenleme ve ekleme yapabilirsin.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Film_Listem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection connection = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\alkan\Desktop\C#\FilmListem\Film_Listesi.xls;Extended Properties ='Excel 8.0;HDR=YES'");   
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ShowMovie_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from [movieList$]", connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+            connection.Close();
+        }
+
+        private void btn_AddMovie_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            OleDbCommand cmd = new OleDbCommand("insert into [movieList$] values (@p1,@p2,@p3,@p4,@p5,@p6)", connection);
+            cmd.Parameters.AddWithValue("@p1", textBox1.Text);
+            cmd.Parameters.AddWithValue("@p2", textBox2.Text);
+            cmd.Parameters.AddWithValue("@p3", textBox3.Text);
+            cmd.Parameters.AddWithValue("@p4", textBox4.Text);
+            cmd.Parameters.AddWithValue("@p5", textBox5.Text);
+            cmd.Parameters.AddWithValue("@p6", richTextBox1.Text);
+
+            cmd.ExecuteNonQuery();
+            connection.Close();
+                    
+            MessageBox.Show("Kaydedildi!", "Tamamlandı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            richTextBox1.Clear();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Datagridde herhangi bir hücreye tıkladığımızda o hücrenin bulunduğu satırdaki tüm değerleri chosen_area ya eeşitledi
+            int chosen_area = dataGridView1.SelectedCells[0].RowIndex; 
+            string name = dataGridView1.Rows[chosen_area].Cells[0].Value.ToString();  // seçilen hücrenin 0. indeksini name değişkenine atadı
+            string date = dataGridView1.Rows[chosen_area].Cells[1].Value.ToString();
+            string type = dataGridView1.Rows[chosen_area].Cells[2].Value.ToString();
+            string imdb = dataGridView1.Rows[chosen_area].Cells[3].Value.ToString();
+            string my_r = dataGridView1.Rows[chosen_area].Cells[4].Value.ToString();
+            string description = dataGridView1.Rows[chosen_area].Cells[5].Value.ToString();
+
+            textBox1.Text = name;
+            textBox2.Text = date;
+            textBox3.Text = type;
+            textBox4.Text = imdb;
+            textBox5.Text = my_r;
+            richTextBox1.Text = description;
+
+        }
+
+        private void btn_FindMovie_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            // ismine göre filmi çekip datagride yazdırıyoruz. Kitap ismini de bir değişkene bağlamak yerine direkt olarak Texbox1 yani movie name kısmından alıyoruz
+            //Burada data adapter kullanmamızın sebebi da.fill komutu ile datatable doldurup, datagride yazdırabilmek
+            //Ek olarak oledbcommand kullanmadan aslında içerideki komutu direkt oledbadapter içine de yazarak işlemimizi gerçekleştirebiliriz
+            OleDbCommand cmd = new OleDbCommand("select * from [movieList$] where Movie_Name like '%" + textBox1.Text + "%'", connection);
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+            connection.Close();
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            richTextBox1.Clear();
+        }
+
+        private void btn_EditMovie_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                
+                OleDbCommand cmd = new OleDbCommand("update [movieList$] set  Type ='" + textBox3.Text + 
+                    "', Imdb='" + textBox4.Text + "', my_R='" + textBox5.Text + "',Descriptions='" + richTextBox1.Text +
+                    "' where Movie_Name='" + textBox1.Text + "'", connection);
+               
+                /*
+                OleDbCommand cmd = new OleDbCommand("update [movieList$] set Type=@p3, Imdb=@p4, my_R=@p5, Descriptions=@p6 where Movie_Name=@p1",connection);
+                cmd.Parameters.AddWithValue("@p1", textBox1.Text);
+                //cmd.Parameters.AddWithValue("@p2", textBox2.Text);
+                cmd.Parameters.AddWithValue("@p3", textBox3.Text);
+                cmd.Parameters.AddWithValue("@p4", textBox4.Text);
+                cmd.Parameters.AddWithValue("@p5", textBox5.Text);
+                cmd.Parameters.AddWithValue("@p6", richTextBox1.Text);
+                */
+
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+      
+    }
+}
